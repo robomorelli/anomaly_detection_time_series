21fbe09fda42a4af08261793b27724a2b2f3eae4
Roberto Morelli <roberto.morelli7@unibo.it>
2023-03-20 12:32:18 +0100

    lstm vae with ,custom or mse loss and vanilla kld

--- main.py
+++ main.py
@@ -192,7 +192,7 @@ def main(args1, args2):
 if __name__ == '__main__':
 
     parser1 = argparse.ArgumentParser()
-    parser1.add_argument("--architecture", default='lstm_vae', help="[lstm, lstm_ae, lstm_vae,"
+    parser1.add_argument("--architecture", default='lstm_ae', help="[lstm, lstm_ae, lstm_vae,"
                                                                 " lstm_vae_vanilla, conv_ae, conv_ae1D")
     #dataset
     parser1.add_argument("--columns", default=columns, help="columns imported from config, [columns, columns_third_wheel]")
@@ -227,7 +227,7 @@ if __name__ == '__main__':
     parser1.add_argument("--embedding_dim", default=32, help="s")
     parser1.add_argument("--n_layers_1", default=1, help="")
     parser1.add_argument("--n_layers_2", default=1, help="")
-    #parser1.add_argument("--no_latent",  action='store_const', const=False, default=False)
+    parser1.add_argument("--no_latent",  action='store_const', const=False, default=False)
     parser1.add_argument("--latent_dim", default=100, help="")
 
     # lstm vae architecture only
